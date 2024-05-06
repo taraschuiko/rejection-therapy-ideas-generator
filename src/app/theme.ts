bac3b658abdeef96e4579b4155cec511474307bb
Taras Chuiko <taraschuiko@gmail.com>
2024-05-06 21:11:08 +0200

    Added new design

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,53 +1,59 @@
 "use client";
 
-import { createTheme } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#f5009b",
-    },
-    secondary: {
-      main: "#00dbeb",
-    },
-  },
-  typography: {
-    fontFamily: "Jaro, sans-serif",
-    h1: {
-      fontWeight: 900,
-    },
-  },
-  components: {
-    MuiTypography: {
-      defaultProps: {
-        color: "#a323d1",
+const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: "#f5009b",
+      },
+      secondary: {
+        main: "#00dbeb",
       },
     },
-    MuiChip: {
-      styleOverrides: {
-        root: {
-          borderRadius: 0,
-          boxShadow: "none",
+    typography: {
+      fontFamily: "VT323, monospace",
+    },
+    components: {
+      MuiTypography: {
+        defaultProps: {
+          color: "#a323d1",
+          lineHeight: 0.8,
+        },
+      },
+      MuiChip: {
+        defaultProps: {
+          component: "button",
+        },
+        styleOverrides: {
+          root: {
+            borderRadius: 0,
+            boxShadow: "none",
+          },
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            borderRadius: 0,
+            boxShadow: "none",
+            "&:hover": {
+              boxShadow: "none",
+            },
+          },
+        },
+      },
+      MuiCard: {
+        styleOverrides: {
+          root: {
+            borderRadius: 0,
+            boxShadow: "none",
+          },
         },
       },
     },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 0,
-          boxShadow: "none",
-        },
-      },
-    },
-    MuiCard: {
-      styleOverrides: {
-        root: {
-          borderRadius: 0,
-          boxShadow: "none",
-        },
-      },
-    },
-  },
-});
+  })
+);
 
 export default theme;
